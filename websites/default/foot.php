faf9ef1add3ab4b43bfc35ec308bcae9795a5723
Peter-Mwendia <ptermwendia@gmail.com>
2024-01-31 19:00:21 +0300

    Create foot.php

---
+++ websites/default/foot.php
@@ -0,0 +1,7 @@
+<!--Template file, used to make the footer and for the html layouts closing tags-->
+</main>
+		<footer>
+			&copy; Fotheby's 2024. All rights reserved.
+		</footer>
+	</body>
+</html>
